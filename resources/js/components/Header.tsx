f03670668a44365119d9d385e4375c325374eb14
Luis Guillermo Pérez Rodríguez <luisguillermo.rodriguez32@gmail.com>
2025-05-21 15:49:29 -0400

    optimize png

--- resources/js/components/Header.tsx
+++ resources/js/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="flex items-center p-4 shadow">
             <Link href={route('home')} className="flex items-center gap-2">
-                <img src="/favicon.png" alt="Logo" className="size-10" />
+                <img loading="lazy" src="/favicon.png" alt="Logo" className="size-10" />
                 <h1 className="text-lg font-semibold">EcoHoja</h1>
             </Link>
 
