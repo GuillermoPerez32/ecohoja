d61d0021bb144dd2119fdf0dab93f8d80b40adba
Luis Guillermo Pérez Rodríguez <luisguillermo.rodriguez32@gmail.com>
2025-05-21 16:06:21 -0400

    whatsapp button in another tab

--- resources/js/components/support-button.tsx
+++ resources/js/components/support-button.tsx
@@ -1,7 +1,12 @@
 const SupportButton = () => {
     return (
         <div className="fixed right-4 bottom-4 z-50">
-            <a className="bg-card border-muted-foreground flex items-center gap-2 rounded-lg border p-2 shadow" href="https://wa.me/+5356713029">
+            <a
+                className="bg-card border-muted-foreground flex items-center gap-2 rounded-lg border p-2 shadow"
+                href="https://wa.me/+5356713029"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 <img src="/whatsapp.png" alt="Whatsapp" className="size-6" />
                 <span className="font-semibold">¿Necesitas ayuda?</span>
             </a>
